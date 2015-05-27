f6663c786429f3d3f1780a0a14d65a2d6df9ee68
Ruslan Ahapkin <r.akhapkin@corp.mail.ru>
2015-05-27 10:45:25 +0300

    Fix issues/653 from PR. Align code.

--- XVim/XVimStatusLine.m
+++ XVim/XVimStatusLine.m
@@ -25,93 +25,95 @@
 @end
 
 @implementation XVimStatusLine{
-    DVTChooserView* _background;
-    NSInsetTextView* _status;
+	DVTChooserView* _background;
+	NSInsetTextView* _status;
 }
 
 - (id)initWithFrame:(NSRect)frame
 {
-    self = [super initWithFrame:frame];
-    if (self) {
-        _background = [NSClassFromString(@"DVTChooserView").alloc init];
-        _background.gradientStyle = 2;  // Style number 2 looks like IDEGlassBarView   
-        [_background setBorderSides:12]; // See DVTBorderedView.h for the meaning of the number
-        _status = [[NSInsetTextView alloc] initWithFrame:NSMakeRect(0, 0, 0, STATUS_LINE_HEIGHT)];
-        _status.backgroundColor = [NSColor clearColor];
-        [_status setEditable:NO];
-        
-        [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(_documentChangedNotification:) name:XVimDocumentChangedNotification object:nil];
-        
-        [self addSubview:_background];
-        [self addSubview:_status];
-    }
-    
-    return self;
+	self = [super initWithFrame:frame];
+	if (self) {
+		_background = [NSClassFromString(@"DVTChooserView").alloc init];
+		_background.gradientStyle = 2;  // Style number 2 looks like IDEGlassBarView   
+		[_background setBorderSides:12]; // See DVTBorderedView.h for the meaning of the number
+		_status = [[NSInsetTextView alloc] initWithFrame:NSMakeRect(0, 0, 0, STATUS_LINE_HEIGHT)];
+		_status.backgroundColor = [NSColor clearColor];
+		[_status setEditable:NO];
+		
+		[[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(_documentChangedNotification:) name:XVimDocumentChangedNotification object:nil];
+		
+		[self addSubview:_background];
+		[self addSubview:_status];
+	}
+	
+	return self;
 }
 
 - (void)dealloc
 {
-    [[NSNotificationCenter defaultCenter] removeObserver:self];
-    
+	[[NSNotificationCenter defaultCenter] removeObserver:self];
+	
 }
 
 - (void)layoutStatus:(NSView*)container
 {
-    DVTFontAndColorTheme* theme = [NSClassFromString(@"DVTFontAndColorTheme") performSelector:@selector(currentTheme)];
+	DVTFontAndColorTheme* theme = [NSClassFromString(@"DVTFontAndColorTheme") performSelector:@selector(currentTheme)];
 	NSFont *sourceFont = [theme sourcePlainTextFont];
 	
-        if (sourceFont.pointSize > MAX_STATUS_LINE_FONT_SIZE) {
-            sourceFont = [NSFont fontWithName:[sourceFont fontName] size:MAX_STATUS_LINE_FONT_SIZE];
-        }
-        
+	if (sourceFont.pointSize > MAX_STATUS_LINE_FONT_SIZE) {
+		sourceFont = [NSFont fontWithName:[sourceFont fontName] size:MAX_STATUS_LINE_FONT_SIZE];
+	}
+	
 	// Calculate inset
 	CGFloat horizontalInset = 0;
 	CGFloat verticalInset = MAX((STATUS_LINE_HEIGHT - [sourceFont pointSize]) / 2, 0);
 	CGSize inset = CGSizeMake(horizontalInset, verticalInset);
 	
-    XVimOptions* options = [[XVim instance] options];
-    CGFloat height;
-    if( options.laststatus == 2 ){
-        height = STATUS_LINE_HEIGHT;
-    } else {
-        height = 0;
-    }
-    NSRect parentRect = [container frame];
-    [self setFrame:NSMakeRect(0, 0, parentRect.size.width, height)];
-    [_background setFrame:NSMakeRect(0, 0, parentRect.size.width, STATUS_LINE_HEIGHT)];
-    [_status setFrame:NSMakeRect(0, 0, parentRect.size.width, STATUS_LINE_HEIGHT)];
+	XVimOptions* options = [[XVim instance] options];
+	CGFloat height;
+	if( options.laststatus == 2 ){
+		height = STATUS_LINE_HEIGHT;
+	} else {
+		height = 0;
+	}
+	NSRect parentRect = [container frame];
+	[self setFrame:NSMakeRect(0, 0, parentRect.size.width, height)];
+	[_background setFrame:NSMakeRect(0, 0, parentRect.size.width, STATUS_LINE_HEIGHT)];
+	[_status setFrame:NSMakeRect(0, 0, parentRect.size.width, STATUS_LINE_HEIGHT)];
 	[_status setFont:sourceFont];
 	[_status setInset:inset];
-    // This is heuristic way...
-    if( [NSStringFromClass([container class]) isEqualToString:@"IDEComparisonEditorAutoLayoutView"] ){
-        // Nothing ( Maybe AutoLayout view does the job "automatically")
-    }else{
-        if( [container subviews].count > 0 ){
-            [[[container subviews] objectAtIndex:0] setFrame:NSMakeRect(0, height, parentRect.size.width, parentRect.size.height-height)];
-        }
-    }
+	[_status setBackgroundColor:[theme sourceTextBackgroundColor]];
+	[_status setTextColor:[theme sourcePlainTextColor]];
+	// This is heuristic way...
+	if( [NSStringFromClass([container class]) isEqualToString:@"IDEComparisonEditorAutoLayoutView"] ){
+		// Nothing ( Maybe AutoLayout view does the job "automatically")
+	}else{
+		if( [container subviews].count > 0 ){
+			[[[container subviews] objectAtIndex:0] setFrame:NSMakeRect(0, height, parentRect.size.width, parentRect.size.height-height)];
+		}
+	}
 }
-    
+
 - (void)didContainerFrameChanged:(NSNotification*)notification{
-    // TODO: Find the way to get scrollView from IDESourceCodeEditor class.
-    // Now it is assumed that container view has the scrollView at index 0 of subviews.
-    NSView* container = [notification object];
-    [self layoutStatus:container];
-     
+	// TODO: Find the way to get scrollView from IDESourceCodeEditor class.
+	// Now it is assumed that container view has the scrollView at index 0 of subviews.
+	NSView* container = [notification object];
+	[self layoutStatus:container];
+	
 }
 
 - (void)_documentChangedNotification:(NSNotification *)notification
 {
-    NSString *documentPath = [[notification userInfo] objectForKey:XVimDocumentPathKey];
-    if (documentPath != nil) {
-        [_status setString:documentPath];
-    }
+	NSString *documentPath = [[notification userInfo] objectForKey:XVimDocumentPathKey];
+	if (documentPath != nil) {
+		[_status setString:documentPath];
+	}
 }
 
 - (void)drawRect:(NSRect)dirtyRect
 {
-    // Drawing code here.
-    
+	// Drawing code here.
+	
 }
 
 static char s_associate_key = 0;
